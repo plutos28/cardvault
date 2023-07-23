d3475681102cb4f1f5c248a944adbc85cc02bb2b
plutos28 <gamevictor28@gmail.com>
2023-07-23 18:11:25 +0300

    complete rough signup page

--- templates/signup.html.php
+++ templates/signup.html.php
@@ -1,1 +1,16 @@
-<h1>Sign Up</h1>
+<div class="form-block">
+    <h1>Get a free Cardvault account</h1>
+    <h2>Enter your details between to get a cardvault aaccount. Soon you can safely store your credit card details with us.</h2>
+    <form action="" method="post">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username"><br>
+
+        <label for="password1">Password</label><br>
+        <input type="password" name="password1" id="password1"><br>
+
+        <label for="password2">Password Confirmation</label><br>
+        <input type="password" name="password2" id="password2"><br>
+
+        <button type="submit">Sign Up</button>
+    </form>
+</div>
