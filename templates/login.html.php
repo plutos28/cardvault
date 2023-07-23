feb29d61479b555e23d280446c258db74773b58f
plutos28 <gamevictor28@gmail.com>
2023-07-23 18:16:31 +0300

    add rough login page

--- templates/login.html.php
+++ templates/login.html.php
@@ -1,1 +1,12 @@
-<h1>Log In</h1>
+<div class="form-block">
+    <h1>Log In</h1>
+    <h2>Enter your details between to get a cardVault aaccount. Soon you can safely store your credit card details with us.</h2>
+    <form action="" method="post">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username"><br>
+
+        <label for="password1">Password</label><br>
+        <input type="password" name="password1" id="password1"><br>
+        <button type="submit">Log In</button>
+    </form>
+</div>
