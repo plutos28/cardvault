e221e48a4b9e82438b05c3154be3da083bee7e26
plutos28 <gamevictor28@gmail.com>
2023-07-24 15:57:44 +0300

    add basic designs for card vault page

--- templates/cardvault.html.php
+++ templates/cardvault.html.php
@@ -1,15 +1,17 @@
 <div class="vault">
-    <h1>Vault</h1>
-    <a href="#" hx-get="addcard.php" hx-target="body" hx-swap="beforeend" class="add-card-link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="rgba(255,255,255,1)"></path></svg>
-    Add Card</a>
-
-    <ul>
+    <div class="vault-header">
+        <h1>My Cards</h1>
+        <a href="#" hx-get="addcard.php" hx-target="body" hx-swap="beforeend" class="add-card-link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="rgba(255,255,255,1)"></path></svg>
+        Add Card</a>
+    </div>
+    <div class="vault-cards">
         <?php foreach ($cards as $card): ?>
-            <li><?=htmlspecialchars($card["cardnumber"], ENT_QUOTES, "UTF-8");?></li>
-            <li><?=htmlspecialchars($card["cardholder_name"], ENT_QUOTES, "UTF-8");?></li>
-            <li><?=htmlspecialchars($card["cvv"], ENT_QUOTES, "UTF-8");?></li>
-            <li><?=htmlspecialchars($card["expiration_date"], ENT_QUOTES, "UTF-8");?></li>
-
+            <div class="vault-card">
+                <p><span style="font-weight: bold;">Card Number:</span> <?=htmlspecialchars($card["cardnumber"], ENT_QUOTES, "UTF-8");?></p>
+                <p><span style="font-weight: bold;">Cardholder Name:</span>: <?=htmlspecialchars($card["cardholder_name"], ENT_QUOTES, "UTF-8");?></p>
+                <p><span style="font-weight: bold;">Security Code(CVC/CVV):</span>: <?=htmlspecialchars($card["cvv"], ENT_QUOTES, "UTF-8");?></p>
+                <p><span style="font-weight: bold;">Expiration Date:</span>: <?=htmlspecialchars($card["expiration_date"], ENT_QUOTES, "UTF-8");?></p>
+            </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 </div>
